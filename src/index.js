29407cad4dda139ab3d25b07beed28cb4bd9baeb
Nathan Hannon <nathan@hannon.me>
2021-03-25 09:50:21 -0400

    added state store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { configureStore } from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={configureStore()} >
-			<App />
+		<Provider store={store} >
+			<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+				<App />
+			</PersistGate>
 		</Provider>,
   </React.StrictMode>,
 	document.getElementById('root')
